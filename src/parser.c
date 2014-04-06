4f0b66eaad7ba4bb27fc04bb57e274540af2ca16
mryndzionek <mryndzionek@gmail.com>
2014-04-06 14:26:29 +0200

    Added selftest implementation for 'bool' parameters

--- src/parser.c
+++ src/parser.c
@@ -6,12 +6,19 @@ main(int argc, char **argv)
     int rc;
 
     rc = opts_test(true);
+    if(rc)
+        printf("Selftest - passed\n");
+    else
+        {
+            printf("Selftest - failed\n");
+            return 1;
+        }
     opts_t *op = opts_parse(argc, argv);
     if(op != NULL)
-    {
-        opts_dump(op);
-        opts_destroy(&op);
-    }
+        {
+            opts_dump(op);
+            opts_destroy(&op);
+        }
     else
         printf("failed to parse arguments\n");
 }
